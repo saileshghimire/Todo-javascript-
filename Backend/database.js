33264e22e2943a2fc6f3e6ee3fce2b11dd09fdb8
saileshghimire <saileshghimire1@gmail.com>
2024-05-22 22:15:52 +0545

    database define

---
+++ Backend/database.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+mongoose.connect(
+    'mongodb+srv://sailesh:1234@cluster0.vm8tuzu.mongodb.net/Todo_database'
+);
+
+const TodoSchema = new mongoose.Schema({
+    title: String,
+    description: String,
+    completed: Boolean
+});
+
+const Todos = mongoose.model('Todos', TodoSchema);
+
+model.export = {
+    Todos
+}
--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,6 @@
 const express  = require("express");
 const app = express();
-
+const { createTodo, updateTodo } = require("./types")
 
 app.use(express.json());
 
@@ -10,7 +10,20 @@ app.get("/todos", async function(req,res){
 });
 
 app.post("/todo", async function(req,res){
-    // 
+    try{
+        const createPayload = req.body;
+        const parsePayload = createTodo.safeParse(createPayload);
+        if(!parsePayload.success){
+            res.status(411).json({
+                msg:"you sent the wrong input"
+            });
+            return;
+        }
+    } catch(error){
+        res.json({
+            error:error
+        });
+    }
 });
 
 app.put("/todo/:id", async function(req,res){
