5a81786dbda69dd095758ca6246f65f0471f6bc5
saileshghimire <saileshghimire1@gmail.com>
2024-05-22 23:20:38 +0545

    get and marked completed todo

--- Backend/index.js
+++ Backend/index.js
@@ -7,7 +7,10 @@ app.use(express.json());
 
 
 app.get("/todos", async function(req,res){
-    // 
+    const todos = await Todos.find({});
+    return res.json({
+        todos
+    });
 });
 
 app.post("/todo", async function(req,res){
@@ -22,10 +25,11 @@ app.post("/todo", async function(req,res){
         }
         const newTodo = await Todos.create({
             title: createPayload.title,
-            description: createPayload.description
-        })
+            description: createPayload.description,
+            completed: false
+        });
         return res.json({
-            msg:'courses created successfully',todo: createPayload
+            msg:"courses created successfully"
         });
     } catch(error){
         res.json({
@@ -34,8 +38,30 @@ app.post("/todo", async function(req,res){
     }
 });
 
-app.put("/todo/:id", async function(req,res){
-    // 
+app.put("/completed", async function(req,res){
+    try{
+        const updatePayload = req.body;
+        const parsePayload = updateTodo.safeParse(updatePayload);
+        if(!parsePayload.success){
+            res.status(411).json({
+                msg:"you sent the wrong input"
+            });
+            return;
+        }
+        await Todos.updateOne({
+            _id: req.body.id
+        },{
+            completed:true
+        });
+        res.json({
+            msg:" Todo marked as compleated"
+        })
+        
+    } catch(error){
+        res.json({
+            error:error
+        });
+    }
 });
 
 app.delete("/todo/:id", function(req,res){
